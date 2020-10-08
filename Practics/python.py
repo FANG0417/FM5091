fbf6fddcd315b0cecda016c661cd0afd3fd36070
FANG0417 <fang0417@umn.edu>
2020-10-08 11:32:44 +0800

    homework2 creation

--- Practics/python.py
+++ Practics/python.py
@@ -1,37 +1,10 @@
 import numpy as np
 
-# def isPrime(x): 
-#     i = np.arange(x - 1, 1, -1) # vector of numbers to divide by
-#     print(i) 
-#     xx = np.ones(len(i)) * x # vector of number being tested 
-#     print(xx)
-#     g = np.mod(xx,i) # perform mod to test for prime-ness 
-#     m = np.zeros(len(i)) # if anything = 0, then not prime 
-#     return (sum(g == m) <= 0) # return result of test
+balls = np.arange(100)
 
-# isPrime(6)
+points = np.append(np.arange(1,31), 10*np.ones(70))
 
-# from functools import reduce
-# g = [1,2,3,4,5]
-# m = list(map(lambda x: x ** 2, g)) 
-# print(m)
-# r = reduce(lambda x, y: x + y, g) 
-# print(r)
+Trials = 5
 
-# def map(setvector, target): 
-#     setvector[target] = setvector[target] * 10
-#     if target < len(setvector) - 1: 
-#         return map(setvector, target + 1) 
-#     else: 
-#         return setvector
-# print(map(g,4))
-
-from sympy import *
-
-# x = symbols('x')
-# expr = limit(sin(x)/x,x,0)
-# b = expr.doit()
-# print(expr,b)
-
-Matrix([[1,2,3],[4,5,6],[7,8,0]]).rref()
+print(points)
 
